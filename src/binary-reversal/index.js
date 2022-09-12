a72640557d8426bc71204ac893dc133ac4230619
Your Name <you@example.com>
2022-09-12 21:29:35 +0100

    my challenge 3

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,23 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+
+    function binaryReversal(value) {
+        // convert value to binary
+        let binary = Number(value).toString(2);
+
+        // pad the binary with 0s to make it 8 digits
+        let paddedBinary = binary.padStart(8, '0');
+
+        // reverse the padded binary
+        let reversedBinary = paddedBinary.split('').reverse().join('');
+
+        // convert the reversed binary to decimal (base 10)
+        let decimalB = parseInt(reversedBinary, 2)
+
+        return decimalB.toString();
+        
+    }
+console.log(binaryReversal('47'));
 
 module.exports = binaryReversal;
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,35 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+    
+    //check if haystack is an array of arrays
+    if(typeof(haystack[0]) == "object") {
+        let column;
+        let row;
+
+        //loop through haystack to find the row that contains the needle
+    for (let i = 0; i < haystack.length; i++) {
+        
+        //loop through each row to find the column that contains the needle
+        for (let j = 0; j < haystack[i].length; j++) {
+            
+            //if the needle is found, store the column and row
+            if (haystack[i][j] === needle) {
+                column = haystack[i].lastIndexOf(needle);
+                row = i;
+                
+            }
+        }
+    }
+    //check if needle is in any row and return the column and row
+    if (row == undefined) return -1;
+    return [row, column];   
+    }
+
+    //return column or needle index if haystack is not an array of arrays
+    else {
+        return haystack.lastIndexOf(needle);
+    }
+}
+console.log(listSorting(6, [1, 2, 3, 4, 6]));
 
 module.exports = listSorting;
+
